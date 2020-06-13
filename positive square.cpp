187fa79056c5568c722abe8574dfe74b34120e03
rakshishetty <65613030+rakshishetty@users.noreply.github.com>
2020-06-13 17:29:20 +0530

    Add files via upload

---
+++ positive square.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int n,i=1,sum=0;
+printf("Enter the no.of elements : ");
+scanf("%d" , &n);
+printf("%d", n);
+for(i=0;i<=n;i++)
+{
+sum=sum+1*1;
+}
+printf(" \nThe sum of positive square elements in an array is %d", sum);
+return 0;
+}
