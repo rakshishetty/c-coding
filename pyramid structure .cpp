705ee909a70f6180770bb82f20bddd0f2a70becc
rakshishetty <65613030+rakshishetty@users.noreply.github.com>
2020-06-16 17:18:07 +0530

    Add files via upload

---
+++ pyramid structure .cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int row, c, n;
+printf("Enter the number of rows in pyramid of stars to print: ");
+scanf("%d", &n);
+printf("%d\n",n);
+printf("The pyramid structure is: \n");
+for (row= 1; row<=n; row++)
+{
+for (c= 1;c<=n-row; c++)
+printf(" ");
+for (c= 1;c<= 2*row-1; c++)
+printf("*");
+printf("\n") ;
+}
+return 0;
+}
