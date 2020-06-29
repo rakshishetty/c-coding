17b0787e4ae4d9840bca803a51b6bf808c585930
rakshishetty <65613030+rakshishetty@users.noreply.github.com>
2020-06-29 19:32:18 +0530

    Add files via upload

---
+++ passing parameter by call by value.c
@@ -0,0 +1,16 @@
+//passing parameter by call by value
+#include<stdio.h>
+int add(int i, int j);
+void main()
+{
+int sum, a=30, b=40;
+sum=add(a,b);
+printf("Sum is %d", sum);
+}
+int add(int i, int j)
+{
+int sum;
+sum=i+j;
+return sum;
+}
+
