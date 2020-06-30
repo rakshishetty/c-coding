ab37cff586481ff26dee4f50471fcb8b0f6175af
rakshishetty <65613030+rakshishetty@users.noreply.github.com>
2020-06-30 17:55:49 +0530

    Add files via upload

---
+++ function without parameter without return type.c
@@ -0,0 +1,14 @@
+//function without parameter without return type
+#include<stdio.h>
+int add();
+void main()
+{
+add();
+}
+int add()
+{
+int sum, i=30, j=40 ;
+sum=i+j;
+printf("Sum is %d", sum);
+}
+
